aae6249acb72f6576a806a451cb9904ba2c59f8c
Pete Voulgaris <petevoulgaris@gmail.com>
2024-08-06 13:05:18 -0400

    updated project to latest version

--- az204-auth/Program.cs
+++ az204-auth/Program.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿
+
+
 using Microsoft.Identity.Client;
 
-namespace az204_auth
-{
-    class Program
-    {
-        private const string _clientId = "8a992334-1824-4a74-9725-5d1cd4fdf985";
-        private const string _tenantId = "9fc5d2bb-7141-4bc6-927b-ef3cc26c4993";
+DotNetEnv.Env.Load();
 
-        public static async Task Main(string[] args)
-        {
-            var app = PublicClientApplicationBuilder
-                .Create(_clientId)
-                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
-                .WithRedirectUri("http://localhost")
-                .Build(); 
-            string[] scopes = { "user.read" };
-            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
-
-            Console.WriteLine($"Token:\t{result.AccessToken}");
-        }
-    }
-}
+var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
 
 
+var app = PublicClientApplicationBuilder
+    .Create(clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build(); 
+string[] scopes = { "user.read" };
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+WriteLine($"Token:\t{result.AccessToken}");
