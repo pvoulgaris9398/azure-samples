29283e69462c8b05610266d2dd37b5b49ccadc67
Pete Voulgaris <petevoulgaris@gmail.com>
2025-01-01 15:49:43 -0500

    started reworking blob sample

---
+++ Azure Storage/Azure Blob Storage/src/AzureBlobStorageExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+namespace AzureBlobStorageExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+
+
+        public BlobServiceClient GetBlobServiceClient(string accountName)
+        {
+            BlobServiceClient client = new(
+                new Uri("https://{accountName}.blob.core.windows.net"),
+                new DefaultAzureCredential());
+            return client;
+
+        }
+    }
+}
