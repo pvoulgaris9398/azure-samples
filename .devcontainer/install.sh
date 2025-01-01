121476b5b3361fa1b888e1c86a7cfc91a5952195
Pete Voulgaris <petevoulgaris@gmail.com>
2025-01-01 18:17:03 -0500

    added some notes on 'Core Tools' on linux ubuntu

---
+++ .devcontainer/install.sh
@@ -0,0 +1,15 @@
+# https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=linux%2Cisolated-process%2Cnode-v4%2Cpython-v2%2Chttp-trigger%2Ccontainer-apps&pivots=programming-language-csharp
+
+# Thursday, 1/1/25:
+# Need to do these steps on my linux machine
+# Make them part of the dev container build?
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs 2>/dev/null)-prod $(lsb_release -cs 2>/dev/null) main" > /etc/apt/sources.list.d/dotnetdev.list'
+
+sudo apt-get update
+
+sudo apt-get install azure-functions-core-tools-4
